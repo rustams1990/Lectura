8f1777fa24bf3f7d481cc0d42d6eec932c5ecce8
rustams1990 <113258774+rustams1990@users.noreply.github.com>
2026-08-25 02:50:02 +0500

    chore: checkpoint bump version to v2.99.94

--- server.ts
+++ server.ts
@@ -19,7 +19,7 @@ import translateRouter from "./routes/translate.ts";
 import { startBackupScheduler } from "./server/backupService.ts";
 import { APP_VERSION } from "./src/version.ts";
 
-// Lectura Server Entry v2.99.93
+// Lectura Server Entry v2.99.94
 dotenv.config();
 
 async function startServer() {
